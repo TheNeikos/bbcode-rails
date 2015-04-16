87ca3b40ec28738f5ac0910b88bb8bc484cc0c34
Marcel Müller <neikos@neikos.email>
2015-04-16 02:53:14 +0200

    Removed logging messages

--- lib/bbcode-rails/railtie.rb
+++ lib/bbcode-rails/railtie.rb
@@ -2,11 +2,6 @@ module BBCode
   class Railtie < Rails::Railtie
     initializer 'bbcode.autoload', :before => :set_autoload_paths do |app|
       app.config.autoload_paths << BBCode::PATH
-      puts "Loaded BBCode at #{BBCode::PATH}"
-    end
-
-    console do
-      puts "Yooo"
     end
   end
 end
